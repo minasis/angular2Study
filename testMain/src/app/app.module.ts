57a4e263550f8d34de2122b188d2ef751ccb77d7
mononic <gkgk100.gg@gmail.com>
2017-04-06 20:06:34 +0900

    ㅌㅌㅌㅌㅌ

--- testMain/src/app/app.module.ts
+++ testMain/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Swiper1Component } from './swiper1/swiper1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Swiper1Component
   ],
   imports: [
     BrowserModule,
---
+++ testMain/src/app/swiper1/swiper1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-swiper1',
+  templateUrl: './swiper1.component.html',
+  styleUrls: ['./swiper1.component.css']
+})
+export class Swiper1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
